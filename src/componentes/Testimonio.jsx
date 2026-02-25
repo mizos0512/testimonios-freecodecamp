8a00f65c52a50a5f13c37fbe77d385e6b06610e3
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-02-25 22:28:35 +0100

    Se modifica Testimonio.jsx

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -3,8 +3,8 @@ import React from   'react';
 function Testimonio( ) {
     return (
         
-        <div className="contenedor-testimonio"> 
-            <img className="imagen-testimonio" src="" alt="" />
+        <div className='contenedor-testimonio'> 
+            <img className="imagen-testimonio" src='' alt='' />
         </div>
 
     );
