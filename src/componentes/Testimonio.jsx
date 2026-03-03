89925694d7880345cb63656427b9389beaf7a6cb
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-03-03 23:18:56 +0100

    Modificando Testimonio.jsx

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,15 +1,22 @@
-import React from   'react';
+import React from 'react';
 import Emma from '../imagenes/testimonio-emma.png';
 
-function Testimonio( ) {
-    return (
-        
-        <div className='contenedor-testimonio'> 
-            <img className="imagen-testimonio" src={'Emma'} alt='Foto de Emma' />
-        </div>
-
-    );
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'> 
+      <img 
+        className='imagen-testimonio' 
+        src={Emma} 
+        alt='Foto de Emma' 
+      />
+      
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>  
+        <p className='texto-testimonio'>{props.testimonio}</p>
+      </div> 
+    </div>
+  );
 }
 
 export default Testimonio;
-
