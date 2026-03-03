1de6bbd5c494d3af7452f24bc370bba8eb9cbd05
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-03-03 22:39:50 +0100

    Creando estructura del componente Testimonio.jsx

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,10 +1,11 @@
 import React from   'react';
+import Emma from '../imagenes/testimonio-emma.png';
 
 function Testimonio( ) {
     return (
         
         <div className='contenedor-testimonio'> 
-            <img className="imagen-testimonio" src='' alt='' />
+            <img className="imagen-testimonio" src={'Emma'} alt='Foto de Emma' />
         </div>
 
     );
