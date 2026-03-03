d387c33c6d20b67880d507fd7b98a8460e46c8bf
Reinaldo Díaz Mizos <162900838+mizos0512@users.noreply.github.com>
2026-03-03 23:17:59 +0100

    Creando archivo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Hola React!</h1>
-      <p>Ya se está renderizando ✅</p>
+    <div className="App">
+    
     </div>
-  );
+  )
 }
+
+export default App;
+
+
